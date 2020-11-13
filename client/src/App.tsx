ad6a50ad2cf4e04f55ddeb3815c0bdcf1d2bbc44
Cristian Cazan <cristian.cazan@qubiz.com>
2020-11-13 16:50:06 +0200

    Add first date picker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import DatePicker from 'react-datepicker';
 
 function App() {
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <div>
       <Container fluid>
         <Row>
           <Col xs={{ span: 8, offset: 2 }}>
-            Hello
+            <DatePicker selected={startDate} onChange={(date: Date) => date && setStartDate(date)} />
           </Col>
         </Row>
       </Container>
