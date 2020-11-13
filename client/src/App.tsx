af223fe943b83a64b3aa82d920eaf516667132f9
Cristian Cazan <cristian.cazan@qubiz.com>
2020-11-13 15:59:48 +0200

    Add react bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function App() {
-  console.log(process.env);
   return (
     <div>
-      Salut
+      <Container fluid>
+        <Row>
+          <Col xs={{ span: 8, offset: 2 }}>
+            Hello
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
